117bbeb90c4f0ce3b6ac14f47f31ada5e89922d3
Paul Cruse <paul.cruse@syngenta.com>
2021-04-27 12:22:14 -0500

    better logger

--- tests/syngenta_digital_alc/common/test_logger.py
+++ tests/syngenta_digital_alc/common/test_logger.py
@@ -4,20 +4,42 @@ from unittest import TestCase, mock
 from syngenta_digital_alc.common import logger
 
 
-@mock.patch.dict(os.environ, {'RUN_MODE': 'SEE-LOGS'})
 class LoggerTest(TestCase):
 
+    @mock.patch.dict(os.environ, {'RUN_MODE': 'SEE-LOGS'})
     def test_logger_logs_simple(self):
-        logger.log(level='ERROR', log={'error': 'test'}, trace=True)
+        logger.log(level='ERROR', log={'error': 'test-simple'}, trace=True)
 
+    @mock.patch.dict(os.environ, {'RUN_MODE': 'SEE-LOGS', 'STAGE': 'local'})
+    def test_logger_logs_simple_local(self):
+        logger.log(level='ERROR', log={'error': 'test-simple-local'}, trace=True)
+
+    @mock.patch.dict(os.environ, {'RUN_MODE': 'SEE-LOGS'})
     def test_logger_logs_error(self):
         try:
-            raise Exception('error')
+            raise Exception('error-object')
         except Exception as error:
             logger.log(level='ERROR', log=repr(error), trace=True)
 
+    @mock.patch.dict(os.environ, {'RUN_MODE': 'SEE-LOGS'})
     def test_logger_logs_error_as_object(self):
         try:
-            raise 'error'
+            raise 'error-string'
         except Exception as error:
             logger.log(level='ERROR', log={'error': repr(error), 'request': 'request'}, trace=True)
+
+    @mock.patch.dict(os.environ, {'RUN_MODE': 'SEE-LOGS', 'STAGE': 'local', 'LOG_LEVEL': 'ERROR'})
+    def test_logger_logs_ignore_info(self):
+        logger.log(level='INFO', log={'INFO': 'ignore'}, trace=True)
+
+    @mock.patch.dict(os.environ, {'RUN_MODE': 'SEE-LOGS', 'STAGE': 'local', 'LOG_LEVEL': '3'})
+    def test_logger_logs_ignore_info_numeric(self):
+        logger.log(level='INFO', log={'INFO': 'ignore-numeric'}, trace=True)
+
+    @mock.patch.dict(os.environ, {'RUN_MODE': 'SEE-LOGS', 'STAGE': 'local', 'LOG_LEVEL': 'INFO'})
+    def test_logger_logs_see_info(self):
+        logger.log(level='INFO', log={'INFO': 'see'}, trace=True)
+
+    @mock.patch.dict(os.environ, {'RUN_MODE': 'SEE-LOGS', 'STAGE': 'local', 'LOG_LEVEL': '1'})
+    def test_logger_logs_see_info_numeric(self):
+        logger.log(level='INFO', log={'INFO': 'see-numeric'}, trace=True)
