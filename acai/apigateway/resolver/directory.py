4b057c31ccdd480b1b1fd4e4ab14823ad75931a2
Paul Cruse III <paulcruse3@gmail.com>
2023-02-27 10:35:10 -0600

    almost working

--- acai/apigateway/resolver/directory.py
+++ acai/apigateway/resolver/directory.py
@@ -5,7 +5,8 @@ class Directory: # pylint: disable=unused-private-member
 
     def __init__(self, **kwargs):
         self.__base_path = Importer.clean_path(kwargs['base_path'])
-        self.__importer = Importer(handlers=kwargs['handler_path'], mode='directory')
+        self.__handler_path = kwargs['handler_path']
+        self.__importer = Importer(handlers=self.__handler_path, mode='directory')
 
     def resolve(self, request):  # pylint: disable=unused-argument
         handler_paths = self.__importer.list_files_in_handler_path()
