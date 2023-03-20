85c039d9253ddcbbab14e8d3649dd2345f8e6f81
Paul Cruse III <paulcruse3@gmail.com>
2023-03-20 14:03:55 -0500

    need to change the middleware

--- tests/mocks/mock_middleware.py
+++ tests/mocks/mock_middleware.py
@@ -10,5 +10,5 @@ def mock_with_auth(request, response, requirements):
     mock_with_auth.has_been_called = True
 
 
-def mock_on_error(request, response, error_message):
+def mock_on_error(request, response):
     mock_on_error.has_been_called = True
