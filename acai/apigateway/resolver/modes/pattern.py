92df05543a9076e27e2c80abb75a4282d5fc3ed8
Paul Cruse III <paulcruse3@gmail.com>
2023-03-29 16:51:12 -0500

    trying to pass pylint

--- acai/apigateway/resolver/modes/pattern.py
+++ acai/apigateway/resolver/modes/pattern.py
@@ -3,9 +3,9 @@ from acai.apigateway.resolver.modes.base import BaseModeResolver
 
 class PatternModeResolver(BaseModeResolver):
 
-    def __init__(self, **kwargs):  # pylint: disable=unused-private-member
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.__handler_pattern = kwargs['handlers']
+        # self.__handler_pattern = kwargs['handlers']
 
     def _get_file_and_import_path(self, request_path):
         pass
