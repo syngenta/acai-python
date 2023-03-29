b9ba5c996e065918f353545f7e1facd287e31948
Paul Cruse III <paulcruse3@gmail.com>
2023-03-29 11:53:59 -0500

    finished cacher

---
+++ acai/apigateway/cacher.py
@@ -0,0 +1,30 @@
+from collections import OrderedDict
+
+
+class Cacher:
+    CACHE_ALL = 'all'
+    CACHE_STATIC = 'static-only'
+    CACHE_DYNAMIC = 'dynamic-only'
+
+    def __init__(self, **kwargs):
+        self.__cache = OrderedDict()
+        self.__size = kwargs.get('size', 128)
+        self.__mode = kwargs.get('cache_mode', self.CACHE_ALL)
+
+    def get(self, method_path):
+        if method_path not in self.__cache:
+            return None
+        self.__cache.move_to_end(method_path)
+        return self.__cache[method_path]
+
+    def put(self, method_path, endpoint):
+        if self.__size is None:
+            return
+        if endpoint.is_dynamic and self.__mode == self.CACHE_STATIC:
+            return
+        if not endpoint.is_dynamic and self.__mode == self.CACHE_DYNAMIC:
+            return
+        self.__cache[method_path] = endpoint
+        self.__cache.move_to_end(method_path)
+        if self.__size != 0 and len(self.__cache) > self.__size:
+            self.__cache.popitem(last=False)
---
+++ tests/acai/apigateway/test_cacher.py
@@ -0,0 +1,105 @@
+import unittest
+import logging
+
+from acai.apigateway.cacher import Cacher
+from acai.apigateway.endpoint import Endpoint
+from acai.apigateway.importer import Importer
+
+
+class CacherTest(unittest.TestCase):
+    handler_path = 'tests/mocks/cacher/directory_handlers'
+
+    def setUp(self):
+        self.importer = Importer(handlers=self.handler_path, mode='directory')
+
+    def test_cache_settings(self):
+        cacher = Cacher()
+        self.assertTrue(cacher.CACHE_ALL == 'all')
+        self.assertTrue(cacher.CACHE_STATIC == 'static-only')
+        self.assertTrue(cacher.CACHE_DYNAMIC == 'dynamic-only')
+
+    def test_cache_put(self):
+        file_path = f'/{self.importer.handlers_path_abs}/basic.py'
+        import_path = 'tests.mocks.cacher.directory_handlers.basic'
+        endpoint_module = self.importer.import_module_from_file(file_path, import_path)
+        endpoint = Endpoint(endpoint_module, 'post')
+        cacher = Cacher()
+        try:
+            cacher.put('post::/unit-test/v1/cacher/basic', endpoint)
+            self.assertTrue(True)
+        except Exception as exception:
+            logging.exception(exception)
+            self.assertTrue(False)
+
+    def test_cache_put_and_get(self):
+        file_path = f'/{self.importer.handlers_path_abs}/basic.py'
+        import_path = 'tests.mocks.cacher.directory_handlers.basic'
+        endpoint_module = self.importer.import_module_from_file(file_path, import_path)
+        endpoint = Endpoint(endpoint_module, 'patch')
+        cacher = Cacher()
+        cacher.put('patch::/unit-test/v1/cacher/basic', endpoint)
+        cached_endpoint = cacher.get('patch::/unit-test/v1/cacher/basic')
+        self.assertTrue(cached_endpoint.has_requirements)
+
+    def test_cache_get_miss(self):
+        cacher = Cacher()
+        endpoint = cacher.get('get::/unit-test/v1/cacher/basic')
+        self.assertTrue(endpoint is None)
+
+    def test_cache_lru_sequence_size_reaches_capacity_and_result_in_miss(self):
+        file_path = f'/{self.importer.handlers_path_abs}/basic.py'
+        import_path = 'tests.mocks.cacher.directory_handlers.basic'
+        endpoint_module = self.importer.import_module_from_file(file_path, import_path)
+        post_endpoint = Endpoint(endpoint_module, 'post')
+        patch_endpoint = Endpoint(endpoint_module, 'patch')
+        get_endpoint = Endpoint(endpoint_module, 'get')
+        cacher = Cacher(size=1)
+        cacher.put('get::/unit-test/v1/cacher/basic', get_endpoint)
+        get_cache_result = cacher.get('get::/unit-test/v1/cacher/basic')
+        self.assertFalse(get_cache_result.has_requirements)
+        cacher.put('patch::/unit-test/v1/cacher/basic', patch_endpoint)
+        cacher.get('patch::/unit-test/v1/cacher/basic')
+        cacher.put('post::/unit-test/v1/cacher/basic', post_endpoint)
+        cacher.get('post::/unit-test/v1/cacher/basic')
+        miss_cache_result = cacher.get('get::/unit-test/v1/cacher/basic')
+        self.assertTrue(miss_cache_result is None)
+
+    def test_cache_size_none_always_miss(self):
+        file_path = f'/{self.importer.handlers_path_abs}/basic.py'
+        import_path = 'tests.mocks.cacher.directory_handlers.basic'
+        endpoint_module = self.importer.import_module_from_file(file_path, import_path)
+        get_endpoint = Endpoint(endpoint_module, 'get')
+        cacher = Cacher(size=None)
+        cacher.put('get::/unit-test/v1/cacher/basic', get_endpoint)
+        get_cache_result = cacher.get('get::/unit-test/v1/cacher/basic')
+        self.assertTrue(get_cache_result is None)
+
+    def test_cache_static_only(self):
+        file_path = f'/{self.importer.handlers_path_abs}/basic.py'
+        import_path = 'tests.mocks.cacher.directory_handlers.basic'
+        endpoint_module = self.importer.import_module_from_file(file_path, import_path)
+        get_endpoint = Endpoint(endpoint_module, 'get')
+        cacher = Cacher(cache_mode='static-only')
+        cacher.put('get::/unit-test/v1/cacher/basic', get_endpoint)
+        get_cache_result = cacher.get('get::/unit-test/v1/cacher/basic')
+        self.assertFalse(get_cache_result.has_requirements)
+        patch_endpoint = Endpoint(endpoint_module, 'patch')
+        patch_endpoint.is_dynamic = True
+        cacher.put('patch::/unit-test/v1/cacher/basic', patch_endpoint)
+        patch_cache_result = cacher.get('patch::/unit-test/v1/cacher/basic')
+        self.assertTrue(patch_cache_result is None)
+
+    def test_cache_dynamic_only(self):
+        file_path = f'/{self.importer.handlers_path_abs}/basic.py'
+        import_path = 'tests.mocks.cacher.directory_handlers.basic'
+        endpoint_module = self.importer.import_module_from_file(file_path, import_path)
+        get_endpoint = Endpoint(endpoint_module, 'get')
+        cacher = Cacher(cache_mode='dynamic-only')
+        patch_endpoint = Endpoint(endpoint_module, 'patch')
+        patch_endpoint.is_dynamic = True
+        cacher.put('patch::/unit-test/v1/cacher/basic', patch_endpoint)
+        patch_cache_result = cacher.get('patch::/unit-test/v1/cacher/basic')
+        self.assertTrue(patch_cache_result.has_requirements)
+        cacher.put('get::/unit-test/v1/cacher/basic', get_endpoint)
+        get_cache_result = cacher.get('get::/unit-test/v1/cacher/basic')
+        self.assertTrue(get_cache_result is None)
