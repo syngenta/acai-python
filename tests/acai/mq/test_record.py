33080ef1ba198adaf6ca52e74b997234cc1cfb4e
Paul Cruse III <paulcruse3@gmail.com>
2023-05-24 11:42:36 -0500

    finished testing mq record

--- tests/acai/mq/test_record.py
+++ tests/acai/mq/test_record.py
@@ -13,6 +13,22 @@ class MQRecordTest(unittest.TestCase):
     def test_record_accepts_event(self):
         record = Record(self.basic_record)
         self.assertEqual(record.operation, record.UNKNOWN)
+        self.assertEqual(record.message_id, self.basic_record['messageID'])
+        self.assertEqual(record.message_type, self.basic_record['messageType'])
+        self.assertDictEqual(record.data, json.loads(base64.b64decode(self.basic_record['data'])))
+        self.assertEqual(record.delivery_mode, self.basic_record['deliveryMode'])
+        self.assertEqual(record.reply_to, self.basic_record['replyTo'])
+        self.assertEqual(record.expiration, self.basic_record['expiration'])
+        self.assertEqual(record.record_type, self.basic_record['type'])
+        self.assertEqual(record.priority, self.basic_record['priority'])
+        self.assertEqual(record.correlation_id, self.basic_record['correlationId'])
+        self.assertEqual(record.redelivered, self.basic_record['redelivered'])
+        self.assertEqual(record.destination, self.basic_record['destination'])
+        self.assertEqual(record.properties, self.basic_record['properties'])
+        self.assertEqual(record.time_stamp, self.basic_record['timestamp'])
+        self.assertEqual(record.in_time, self.basic_record['brokerInTime'])
+        self.assertEqual(record.out_time, self.basic_record['brokerOutTime'])
+        self.assertDictEqual(record.body, json.loads(base64.b64decode(self.basic_record['data'])))
 
     def test_record_prints(self):
         try:
