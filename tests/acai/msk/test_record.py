67068f6b460e1b28faf3305b388e66f707ce81f1
Paul Cruse III <paulcruse3@gmail.com>
2023-05-24 11:47:09 -0500

    finished msk record tests

--- tests/acai/msk/test_record.py
+++ tests/acai/msk/test_record.py
@@ -13,6 +13,15 @@ class MSKRecordTest(unittest.TestCase):
     def test_record_accepts_event(self):
         record = Record(self.basic_record)
         self.assertEqual(record.operation, record.UNKNOWN)
+        self.assertEqual(record.topic, self.basic_record['topic'])
+        self.assertEqual(record.partition, self.basic_record['partition'])
+        self.assertEqual(record.offset, self.basic_record['offset'])
+        self.assertEqual(record.time_stamp, self.basic_record['timestamp'])
+        self.assertEqual(record.time_stamp_type, self.basic_record['timestampType'])
+        self.assertEqual(record.key, base64.b64decode(self.basic_record['key']).decode('utf-8'))
+        self.assertDictEqual(record.value, json.loads(base64.b64decode(self.basic_record['value']).decode('utf-8')))
+        self.assertDictEqual(record.body, json.loads(base64.b64decode(self.basic_record['value']).decode('utf-8')))
+        self.assertListEqual(record.headers, self.basic_record['headers'])
 
     def test_record_prints(self):
         try:
