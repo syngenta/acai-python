725cb47e17ee6736d064811ff87aac92e7b2ff2d
Paul Cruse III <paulcruse3@users.noreply.github.com>
2024-04-18 14:24:07 -0400

    Update config_validator.py

--- acai_aws/apigateway/config_validator.py
+++ acai_aws/apigateway/config_validator.py
@@ -15,9 +15,9 @@ class ConfigValidator:
             raise ApiException(code=500, message='auto_validate should be a boolean')
         if kwargs.get('validate_response') and not isinstance(kwargs.get('validate_response'), bool):
             raise ApiException(code=500, message='validate_response should be a boolean')
-        if kwargs.get('cache_size') and not isinstance(kwargs.get('cache_size'), int) and kwargs.get('cache_size') is not None:
+        if kwargs.get('cache_size') and kwargs.get('cache_size') is not None and not isinstance(kwargs.get('cache_size'), int):
             raise ApiException(code=500, message='cache_size should be an int (0 for unlimited size) or None (to disable route caching)')
         if kwargs.get('cache_mode') and kwargs['cache_mode'] not in ('all', 'static-only', 'dynamic-only'):
-            raise ApiException(code=500, message='cache_mode should be a string of the one of the following values: all, static-only, dynamic-only')
+            raise ApiException(code=500, message='cache_mode should be one of the following strings: all, static-only, dynamic-only')
         if kwargs.get('verbose_logging') and not isinstance(kwargs.get('verbose_logging'), bool):
             raise ApiException(code=500, message='verbose_logging should be a boolean')
