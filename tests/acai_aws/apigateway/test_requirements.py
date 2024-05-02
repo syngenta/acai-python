84737b7018fa589d94fd72033151a1f3eb03f654
Paul Cruse III <paulcruse3@gmail.com>
2024-05-01 19:36:40 -0500

    got basic pydantic support working

--- tests/acai_aws/apigateway/test_requirements.py
+++ tests/acai_aws/apigateway/test_requirements.py
@@ -5,7 +5,8 @@ from acai_aws.apigateway.request import Request
 from acai_aws.apigateway.response import Response
 
 from tests.mocks.apigateway import mock_request
-from tests.mocks.apigateway.requirements.basic import post, get, patch, before_call, after_call, call_order
+from tests.mocks.apigateway.requirements import basic
+from tests.mocks.apigateway.router.directory_handlers import pydantic
 
 
 class ApigatewayRequirementsTest(unittest.TestCase):
@@ -26,38 +27,38 @@ class ApigatewayRequirementsTest(unittest.TestCase):
     }
 
     def test_requirements_decorator_has_attribute(self):
-        self.assertTrue(hasattr(post, 'requirements'))
+        self.assertTrue(hasattr(basic.post, 'requirements'))
 
     def test_requirements_runs_before(self):
         request = Request(self.basic_request)
         response = Response()
-        post(request, response)
-        self.assertTrue(before_call.has_been_called)
+        basic.post(request, response)
+        self.assertTrue(basic.before_call.has_been_called)
 
     def test_requirements_runs_after(self):
         request = Request(self.basic_request)
         response = Response()
-        post(request, response)
-        self.assertTrue(after_call.has_been_called)
+        basic.post(request, response)
+        self.assertTrue(basic.after_call.has_been_called)
 
     def test_requirements_runs_in_correct_order(self):
         request = Request(self.basic_request)
         response = Response()
-        post(request, response)
-        self.assertEqual('before', call_order[0])
-        self.assertEqual('after', call_order[1])
+        basic.post(request, response)
+        self.assertEqual('before', basic.call_order[0])
+        self.assertEqual('after', basic.call_order[1])
 
     def test_requirements_passes_after_data_class(self):
         request = Request(self.basic_request)
         response = Response()
-        result = post(request, response)
+        result = basic.post(request, response)
         self.assertEqual(str(self.expected_data_class_result), str(result))
     
     def test_requirements_global_timeout_raises_exception(self):
         request = Request(self.basic_request, None, 1)
         response = Response()
         try:
-            get(request, response)
+            basic.get(request, response)
             self.assertTrue(False)
         except ApiTimeOutException as error:
             self.assertTrue(isinstance(error, ApiTimeOutException))
@@ -67,7 +68,7 @@ class ApigatewayRequirementsTest(unittest.TestCase):
         request = Request(event)
         response = Response()
         try:
-            patch(request, response)
+            basic.patch(request, response)
             self.assertTrue(False)
         except ApiTimeOutException as error:
             self.assertTrue(isinstance(error, ApiTimeOutException))
@@ -77,9 +78,14 @@ class ApigatewayRequirementsTest(unittest.TestCase):
         request = Request(event, None, 10)
         response = Response()
         try:
-            patch(request, response)
+            basic.patch(request, response)
             self.assertTrue(False)
         except ApiTimeOutException as error:
             self.assertTrue(isinstance(error, ApiTimeOutException))
 
-        
+    def test_requirements_with_pydantic_validation_does_not_interfeere(self):
+        expected = {'pydantic_pass': True}
+        request = Request(mock_request.get_basic_passing_for_required_body_validation())
+        response = Response()
+        result = pydantic.post(request, response)
+        self.assertDictEqual(expected, result.raw)    
