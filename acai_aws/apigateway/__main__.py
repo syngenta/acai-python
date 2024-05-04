a7fe75649fb44a0e307dce36098a9521ff0c69bb
Paul Cruse III <paulcruse3@gmail.com>
2024-05-04 03:03:05 -0500

    make things right

--- acai_aws/apigateway/__main__.py
+++ acai_aws/apigateway/__main__.py
@@ -4,7 +4,7 @@ from acai_aws.apigateway.openapi_generator.input.arguments import InputArguments
 from acai_aws.apigateway.openapi_generator.input.validator import InputValidator
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     inputs = InputArguments()
     validator = InputValidator()
     scanner = HandlerScanner(inputs.handlers)
