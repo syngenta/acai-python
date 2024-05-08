5556a9080c2decd8fdd638262447774897060b70
Paul Cruse III <paulcruse3@gmail.com>
2024-05-07 20:22:09 -0500

    getting rid of stuff we don't need

--- tests/mocks/apigateway/router/directory_handlers/unhandled_exception.py
+++ tests/mocks/apigateway/router/directory_handlers/unhandled_exception.py
@@ -5,4 +5,3 @@ from acai_aws.apigateway.requirements import requirements
 def post(request, response):
     response.body = {'router_directory_basic': request.body}
     raise UnknownException(code=418, key_path='crazy_error', message='I am a teapot')
-    return response
